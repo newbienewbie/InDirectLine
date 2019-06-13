cfee10dd17fe59adcfb73133779d114b1d389ba8
newbienewbie <itminus@hotmail.com>
2019-06-13 21:32:12 +0800

    Add comments for InDirectLineSettings

--- Itminus.InDirectLine.Core/Services/InDirectLineOptions.cs
+++ Itminus.InDirectLine.Core/Services/InDirectLineOptions.cs
@@ -1,9 +1,27 @@
 namespace Itminus.InDirectLine.Core.Services{
 
+    /// <summary>
+    /// 
+    /// </summary>
 
     public class InDirectLineSettings{
+
+        /// <summary>
+        /// This is the serive URL exposed to Bot as well as Clients.
+        /// Note the ServiceUrl is different from the `--urls` listened by InDirectLine server. The `--urls` might be used internally (let's say it sits behind a reverse proxy).
+        /// </summary>
+        /// <value></value>
         public string ServiceUrl{get;set;}  = "http://127.0.0.1:3000";
+        /// <summary>
+        /// the message endpoint provided by Bot 
+        /// </summary>
+        /// <value></value>
         public string BotEndpoint {get;set;} = "http://127.0.0.1:3978/api/messages";
+
+        /// <summary>
+        /// count in seconds
+        /// </summary>
+        /// <value></value>
         public int TokenExpiresIn {get;set;} = 36000;
         public int StreamUrlMustBeConnectedIn{get;set;} =60;
 
